b673b2101e7544f3630aedca60899a830b582a37
elseptiawan <septiawanelshandi@gmail.com>
2023-08-31 02:00:09 +0700

    fixing template

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -20,6 +20,9 @@
     <link rel="stylesheet" href="{{ asset('dist/css/vertical-layout/style.css') }}">
     <!-- endinject -->
     <link rel="shortcut icon" href="{{ asset('dist/images/favicon.png') }}" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 <body>
